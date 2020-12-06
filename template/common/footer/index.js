4bb3c93a73838924ff93e547c85828d752347fe8
tension-yao <tension_yao@163.com>
2020-12-06 19:14:21 +0800

    update

--- template/common/footer/index.js
+++ template/common/footer/index.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.--footer-container .back-top .btn').click(() =>{
+  $('.--footer-container .back-top .--btn').click(() =>{
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
