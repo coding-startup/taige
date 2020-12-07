d959f421c3f2562f75576de49d7cc42c14d93fc9
Runbow-Z <785021077@qq.com>
2020-12-07 18:52:37 +0800

    提交文件7

--- pages/page-2/memorabilia/memorabilia.js
+++ pages/page-2/memorabilia/memorabilia.js
@@ -4,19 +4,12 @@
 	});
 })();
 $('#header').load('../../../template/common/header/index.html', function () {
-    $(this).prepend(
-        $('<link/>', { rel: 'stylesheet', href: '../../../assets/css/reset.css' }),
-        $('<link/>', { rel: 'stylesheet', href: '../../../assets/iconfont/iconfont.css' }),
-        $('<link/>', { rel: 'stylesheet', href: '../../../template/common/header/index.css' })
-    ).append(
+    $(this).append(
         $('<script/>', { src: '../../../template/common/header/index.js' })
     )
 })
 $('#footer').load('../../../template/common/footer/index.html', function () {
-    $(this).prepend(
-    	$('<link/>', { rel: 'stylesheet', href: '../../../assets/iconfont/iconfont.css' }),
-        $('<link/>', { rel: 'stylesheet', href: '../../../template/common/footer/index.css' })
-    ).append(
+    $(this).append(
         $('<script/>', { src: '../../../template/common/footer/index.js' })
     )
 })
